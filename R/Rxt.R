a6752a59856ef032cde76f8972abcfa51fe50096
dmkaplan2000 <dmkaplan2000@users.noreply.github.com>
2014-03-31 22:05:24 +0200

    Changing more names

---
+++ R/Rxt.R
@@ -0,0 +1,19 @@
+#' Functions for analyzing the H2 database containing the NXT blockchain
+#' 
+#' This package contains a series of R functions for accessing and analyzing
+#' data in the  NXT blockchain, which is stored in a H2 (java) database.  To use
+#' this package, one must either connect to a copy of the H2 database that is
+#' not in active use by the (NXT) NRS Client or instruct the NRS client to open
+#' the database with the AUTO_SERVER=TRUE option using the nxt.dbUrl property.
+#' 
+#' \tabular{ll}{ Package: \tab Rxt\cr Type: \tab Package\cr Version:
+#' \tab 0.1\cr Date: \tab 2014-03-31\cr License: \tab GPL (>= 2)\cr }
+#' 
+#' @author David M. Kaplan \email{dmkaplan2000@@gmail.com}
+#' 
+#' Maintainer: David M. Kaplan \email{dmkaplan2000@@gmail.com}
+#' @name Rxt
+#' @docType package
+#' @title Functions for analyzing the H2 database containing the NXT blockchain
+#' @keywords package
+NULL
